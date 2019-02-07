e13bbdda2bf2b567135fecb499d5da7d89fe03a3
U_35007842 <U_35007842@PC3>
2019-02-07 12:03:43 +0000

    paso1 y paso2

---
+++ src/prueba/calculadora.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+/**
+ *
+ * @author U_35007842
+ */
+public class calculadora {
+
+   private int operador1;
+   private int operador2;
+   private int operacion;
+   
+    public static void main(String[] args) {
+       
+    }
+    
+}
